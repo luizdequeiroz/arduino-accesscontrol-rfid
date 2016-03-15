1de88c932d22310adbff62a7629f12a9c6ad3cbd
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-03-15 19:35:41 -0300

    Commitando implementação inicial da página Consultar do Controller Administracao. Com problema acerca do header navigation.

--- AccessControl/App_Start/BundleConfig.cs
+++ AccessControl/App_Start/BundleConfig.cs
@@ -16,7 +16,7 @@ namespace AccessControl
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
---
+++ AccessControl/Controllers/AdministracaoController.cs
@@ -0,0 +1,33 @@
+﻿using AccessControl.Models;
+using AccessControl.Models.DAOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccessControl.Controllers
+{
+    public class AdministracaoController : Controller
+    {
+        public ActionResult Consultar(string busca = "")
+        {
+            var usuarios = new UsuarioDao().Listar();
+            if (!string.IsNullOrEmpty(busca))
+                usuarios = usuarios.Where(u => u.Nome.Contains(busca)).ToList();
+            usuarios = usuarios.OrderBy(u => u.Nome).ToList();
+
+            return View(usuarios);
+        }
+
+        public ActionResult Alterar()
+        {
+            return View();
+        }
+
+        public ActionResult Deletar()
+        {
+            return View();
+        }
+    }
+}
