29d430875632bb082e1ffbc76254881525c6eb37
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-03-16 16:30:32 -0300

    Adicionando a connectionString correta para acessar a base de dados no servidor do azure: connectionString=" metadata=res://*/Models.AccessControl.csdl|res://*/Models.AccessControl.ssdl|res://*/Models.AccessControl.msl; provider=System.Data.SqlClient; provider connection string=&quot;data source=tcp:devads.database.windows.net,1433; initial catalog=ACCESSCONTROL; integrated security=False; user id=luizdequeiroz@devads; password=@ProtegoMax; encrypt=True; MultipleActiveResultSets=True; App=EntityFramework&quot;"

--- AccessControl/Models/AccessControl.Designer.cs
+++ AccessControl/Models/AccessControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\luizd\onedrive\documentos\visual studio 2013\Projects\AccessControl\AccessControl\Models\AccessControl.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Repos\AccessControl-desenvolvimento\AccessControl\Models\AccessControl.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
