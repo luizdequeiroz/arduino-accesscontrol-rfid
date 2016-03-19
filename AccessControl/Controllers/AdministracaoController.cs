76e271f7f2cf0dad8ccff66d5dbd3eefd117e01f
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-03-19 18:26:17 -0300

    Commitando alterações para salvar projeto em situação problemática acerca do debug do visual studio, por incrível que pareça.

--- AccessControl/Controllers/AdministracaoController.cs
+++ AccessControl/Controllers/AdministracaoController.cs
@@ -12,69 +12,30 @@ namespace AccessControl.Controllers
 {
     public class AdministracaoController : Controller
     {
-        public List<Usuario> todos = new UsuarioDao().Listar();
-
-        public ActionResult _Busca(string busca = "")
+        public ActionResult Consultar(string busca = "", string pagina = "_Resultado")
         {
-            if (busca == "" && Session["_busca"] != null)
-                busca = Session["_busca"].ToString();
-            else
-                Session["_busca"] = busca;
+            var todos = new UsuarioDao().Listar();
 
-            var usuarios = new List<Usuario>();
-
-            if (Session["rfid"] == null)
+            if(SessionInvalida(todos))
                 return RedirectToAction("Inicio", "Inicio");
-
-            var uSessao = todos.Where(u => u.Rfid.Equals(Session["rfid"])).FirstOrDefault();
-            if (uSessao.Tipo != "Adm")
-                return RedirectToAction("Inicio", "Inicio");
-
             if (Request.IsAjaxRequest())
-            {
-                if (busca.ToLower() == "normal")
-                    usuarios.AddRange(todos.Where(u => u.Tipo.ToLower().Equals("nor")).ToList());
-                if (busca.ToLower().Contains("adm"))
-                    usuarios.AddRange(todos.Where(u => u.Tipo.ToLower().Equals("adm")).ToList());
+                return PartialView(pagina, buscar(busca, todos));
 
-                if (!string.IsNullOrEmpty(busca))
-                {
-                    var massaDeDados = new Hashtable();
-                    foreach (var u in todos)
-                    {
-                        string str = u.Nome + u.Email + u.Descricao + u.Telefone + u.Nascimento;
-                        massaDeDados[u.Rfid] = str;
-                    }
-                    foreach (var us in todos)
-                    {
-                        if (massaDeDados[us.Rfid].ToString().ToLower().Contains(busca.ToLower()))
-                        {
-                            usuarios.Add(todos.Where(u => u.Rfid.Equals(us.Rfid)).FirstOrDefault());
-                        }
-                    }
-                }
-                return PartialView(Session["_page"].ToString(), usuarios.Distinct().OrderBy(u => u.Nome).ToList());
-            }
-
-            return View(usuarios);
-        }
-
-        public ActionResult Consultar()
-        {
-            Session["_page"] = "_Resultado";
             return View();
         }
 
         public ActionResult Atualizar()
         {
-            Session["_page"] = "_Formulario";
-            if (Session["_local"] != null && Session["_local"] == "_edicao")
-                return View();
-            Session["_local"] = "_perfil";
             return View();
         }
 
-        public ActionResult AtualizarGo(Usuario usuario)
+        public ActionResult Formulario(Usuario usuario)
+        {
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Atualizar(Usuario usuario)
         {
             if (ModelState.IsValid)
             {
@@ -119,6 +80,44 @@ namespace AccessControl.Controllers
         public ActionResult Deletar()
         {
             return View();
+        }
+
+        [NonAction]
+        public List<Usuario> buscar(string busca, List<Usuario> todos)
+        {
+            var usuarios = new List<Usuario>();
+
+            if (busca.ToLower() == "normal")
+                usuarios.AddRange(todos.Where(u => u.Tipo.ToLower().Equals("nor")).ToList());
+            if (busca.ToLower().Contains("adm"))
+                usuarios.AddRange(todos.Where(u => u.Tipo.ToLower().Equals("adm")).ToList());
+            if (busca.ToLower() == "todos" || busca.ToLower() == "all")
+                return todos.OrderBy(u => u.Nome).ToList();
+
+            if (!string.IsNullOrEmpty(busca))
+            {
+                var massaDeDados = new Hashtable();
+                foreach (var u in todos)
+                {
+                    string str = u.Nome + u.Email + u.Descricao + u.Telefone + u.Nascimento;
+                    massaDeDados[u.Rfid] = str;
+                }
+                foreach (var us in todos)
+                    if (massaDeDados[us.Rfid].ToString().ToLower().Contains(busca.ToLower()))
+                        usuarios.Add(todos.Where(u => u.Rfid.Equals(us.Rfid)).FirstOrDefault());
+            }
+            return usuarios.Distinct().OrderBy(u => u.Nome).ToList();
+        }
+
+        [NonAction]
+        public bool SessionInvalida(List<Usuario> todos)
+        {
+            if (Session["rfid"] == null)
+                return true;
+            var uSessao = todos.Where(u => u.Rfid.Equals(Session["rfid"])).FirstOrDefault();
+            if (uSessao.Tipo != "Adm")
+                return true;
+            return false;
         }
     }
 }
