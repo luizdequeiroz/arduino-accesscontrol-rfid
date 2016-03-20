e948188e5474ce5800c420da0cc96403e5450c2a
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-03-20 01:09:35 -0300

    Mais implementações no Controller Administrativos e implementação da página do Console SQL, ainda em fase beta, totalmente falho, mas quebra o galho pra selecionar todas as colunas de cada tabela por vez.

--- AccessControl/Controllers/AdministracaoController.cs
+++ AccessControl/Controllers/AdministracaoController.cs
@@ -3,6 +3,9 @@ using AccessControl.Models.DAOs;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -12,25 +15,32 @@ namespace AccessControl.Controllers
 {
     public class AdministracaoController : Controller
     {
-        public ActionResult Consultar(string busca = "", string pagina = "_Resultado")
+        public ActionResult Consultar(string busca = "")
+        {
+            return retornarView(busca, "_Resultado");
+        }
+
+        [NonAction]
+        public ActionResult retornarView(string busca, string pagina)
         {
             var todos = new UsuarioDao().Listar();
 
-            if(SessionInvalida(todos))
+            if (SessionInvalida(todos))
                 return RedirectToAction("Inicio", "Inicio");
             if (Request.IsAjaxRequest())
                 return PartialView(pagina, buscar(busca, todos));
 
-            return View();
+            return View(new List<Usuario>());
         }
 
-        public ActionResult Atualizar()
+        public ActionResult Atualizar(string busca = "")
         {
-            return View();
+            return retornarView(busca, "_Perfis");
         }
 
-        public ActionResult Formulario(Usuario usuario)
+        public ActionResult Formulario(long Rfid)
         {
+            var usuario = new UsuarioDao().Selecionar(Rfid);
             return View(usuario);
         }
 
@@ -54,11 +64,6 @@ namespace AccessControl.Controllers
                     var file = new FileInfo(foto.FileName);
                     if (file.Extension == ".jpg" || file.Extension == ".png" || file.Extension == ".gif")
                     {
-                        if (new UsuarioDao().Selecionar(usuario.Rfid) != null)
-                        {
-                            ModelState.AddModelError("", "Cartão já cadastrado! Volte para o Início e apresente um novo cartão, a sessão atual é de um cartão cadastrado!");
-                            return View();
-                        }
                         new FotoDao().Atualizar(new Foto { Imagem = byts, Rfid = usuario.Rfid });
                         new UsuarioDao().Atualizar(usuario);
                     }
@@ -67,7 +72,7 @@ namespace AccessControl.Controllers
                         ModelState.AddModelError("", "Insira um arquivo de formato válido para imagem (.jpg, .png, .gif)");
                         return View();
                     }
-                    return View("Sucesso", usuario);
+                    return View("Atualizar", new List<Usuario> { usuario });
                 }
                 catch (Exception e)
                 {
@@ -118,6 +123,67 @@ namespace AccessControl.Controllers
             if (uSessao.Tipo != "Adm")
                 return true;
             return false;
+        }
+
+        public ActionResult ConsoleSql(string consulta = "")
+        {
+            if (consulta != "")
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["connForConsole"].ConnectionString;
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(consulta, con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    var reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        var resultados = new List<Object>();
+                        while (reader.Read())
+                        {
+                            if (reader.FieldCount == 8)
+                                resultados.Add(new Usuario
+                                                   {
+                                                       Id = (int)reader.GetValue(0),
+                                                       Nome = (string)reader.GetValue(1),
+                                                       Email = (string)reader.GetValue(2),
+                                                       Descricao = (string)reader.GetValue(3),
+                                                       Telefone = (string)reader.GetValue(4),
+                                                       Nascimento = (string)reader.GetValue(5),
+                                                       Tipo = (string)reader.GetValue(6),
+                                                       Rfid = (long)reader.GetValue(7)
+                                                   });
+                            if (reader.FieldCount == 3)
+                                resultados.Add(new Foto 
+                                                   {
+                                                       Id = (int)reader.GetValue(0),
+                                                       Rfid = (long)reader.GetValue(1),
+                                                       Imagem = null
+                                                   });
+                        }
+                        return PartialView("_ConsoleResult", resultados);
+                    }
+                    else
+                    {
+                        return JavaScript("alert('Não há resultados para esta consulta!')");
+                    }
+                }
+                catch (SqlException sqlex)
+                {
+                    return JavaScript("alert('Erro Sql: " + sqlex.Message + "')");
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                if (Request.IsAjaxRequest())
+                    return PartialView("_ConsoleResult");
+                return View();
+            }
         }
     }
 }
