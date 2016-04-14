ef635df8764797df3ab87f58530297015fecbec6
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-04-14 16:59:31 -0300

    Adicionando a pasta referente à extensão para google chrome (acredito que a resolução do problema da captura da Tag RFID e envio http post para o AccessControl estará nesta solução

---
+++ AccessControl - Extension/POTAC.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    while (true) {
+
+    }
+    var serialPort = require("serialport");
+    serialPort.list(function (err, ports) {
+        ports.forEach(function (port) {
+            console.log(port.comName);
+            console.log(port.pnpId);
+            console.log(port.manufacturer);
+        });
+    });
+
+    alert(listPorts);
+});
