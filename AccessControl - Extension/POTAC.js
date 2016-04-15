bd8dae5b8d513c43711d5e8b715425255be2b1c2
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-04-15 17:11:50 -0300

    Implementação da requisição que será feita pela extensão ao WebService no Arduino 2.0

--- AccessControl - Extension/POTAC.js
+++ AccessControl - Extension/POTAC.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
-    var tag = '123456789'; // para teste, o valor oficial eh ''
-    while (true) {
+    var tag = ''; // para teste, o valor oficial eh ''
+    console.log("Variavel tag declarada!");
+    setInterval(function requestObserver() {
+        console.log("Inicio do intervalo! Tentar fazer a requisicao Ajax...");
         // Consumir a WebService Arduino...
         $.ajax({
             type: "GET",
@@ -13,14 +15,24 @@ $(document).ready(function () {
             success: function (result, jqXHR) {
                 // Interpretando retorno JSON...
                 tag = JSON.parse(result);
+
+                console.log("Sucesso na requisicao");
+
+                $('#Rfid').val(tag);
+                $('form').submit();
             },
             error: function (jqXHR, status) {
-                tag = '123456789'; // para teste, o valor oficial eh ''
-            },
+
+                $.ajax(requestObserver());
+                console.log("Erro na requisicao: " + jqXHR);
+            }
         });
-        if (tag != '')
-            break;
-    }
-    $('#Rfid').val(tag);
-    $('form').submit();
+
+        console.log("Fim da requisicao Ajax!");
+        if (tag == '') {
+
+            console.log("Recarregando a pagina...");
+        }
+    }, 3000);
+
 });
