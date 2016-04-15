582fd2f21a820db46112353a79b600cbaf699c25
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-04-15 11:54:27 -0300

    Implementação da requisição que será feita pela extensão ao WebService no Arduino.

--- AccessControl - Extension/POTAC.js
+++ AccessControl - Extension/POTAC.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
+
+    var tag = '123456789'; // para teste, o valor oficial eh ''
     while (true) {
-
-    }
-    var serialPort = require("serialport");
-    serialPort.list(function (err, ports) {
-        ports.forEach(function (port) {
-            console.log(port.comName);
-            console.log(port.pnpId);
-            console.log(port.manufacturer);
+        // Consumir a WebService Arduino...
+        $.ajax({
+            type: "GET",
+            url: "http://ip.aduino.ethernetshield.ip/", // endereco de teste para forcar o erro
+            //data: "iAmTheExtension",
+            timeout: 3000,
+            contentType: "application/json; charset=utf-8",
+            //dataType: "json",
+            success: function (result, jqXHR) {
+                // Interpretando retorno JSON...
+                tag = JSON.parse(result);
+            },
+            error: function (jqXHR, status) {
+                tag = '123456789'; // para teste, o valor oficial eh ''
+            },
         });
-    });
-
-    alert(listPorts);
+        if (tag != '')
+            break;
+    }
+    $('#Rfid').val(tag);
+    $('form').submit();
 });
